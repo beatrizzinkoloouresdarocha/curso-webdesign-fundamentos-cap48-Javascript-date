02bd19bcaa09e183e88c1063e9f11a6f8b72da0c
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-17 16:00:07 -0300

    acabei de acrecentar mais esta função  function getDATE(str){         return new Date ()     }

--- saveTheDate/js/savethedate.js
+++ saveTheDate/js/savethedate.js
@@ -1,5 +1,9 @@
 (function () {
     const dateEventDom = document.querySelector(".hero-content h1 span").innerText;
     console.log(dateEventDom);
-    
-})();
+
+    function getDATE(str){
+        return new Date ()
+    }
+
+})()
