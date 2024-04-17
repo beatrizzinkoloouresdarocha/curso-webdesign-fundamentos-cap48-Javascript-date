261cdbb3abd21788737d5704632e90605ee9ac7e
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-17 14:07:41 -0300

    concluido todas as etapas e coloquei pra rodar funcionou .

--- introducao.js
+++ introducao.js
@@ -1,9 +1,9 @@
-const data  =new Date ()
-console.log(data)
-console.log(data.getDate())
-console.log(data.getDate())
-console.log(data.getMonth())
-console.log(data.getTime())
-console.log(data.getFullYear())
-console.log(data.getHours())
-console.log(data.getUTHours())
+const data = new Date();
+console.log(data); // Imprime a data e hora completas no console.
+console.log(data.getDay()); // Imprime o dia da semana (0-6, onde 0 é Domingo) no console.
+console.log(data.getDate()); // Imprime o dia do mês (1-31) no console.
+console.log(data.getMonth()); // Imprime o mês (0-11) no console. (Lembre-se que janeiro é 0)
+console.log(data.getTime()); // Imprime o número de milissegundos desde 1º de janeiro de 1970 no console (conhecido como "timestamp").
+console.log(data.getFullYear()); // Imprime o ano (com 4 dígitos) no console.
+console.log(data.getHours()); // Imprime a hora do dia (0-23) no console.
+console.log(data.getUTCHours()); // Imprime a hora em formato UTC (Tempo Universal Coordenado) no console.
