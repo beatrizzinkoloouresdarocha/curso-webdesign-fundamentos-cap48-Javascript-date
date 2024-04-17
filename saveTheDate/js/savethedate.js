65d0e13736b62e00e99591b1ad14f9bc1985b0bf
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-17 16:24:48 -0300

    finalizeri mais uma etapa.

--- saveTheDate/js/savethedate.js
+++ saveTheDate/js/savethedate.js
@@ -1,6 +1,7 @@
 (function () {
     const dateEventDom = document.querySelector(".hero-content h1 span").innerText;
     console.log(dateEventDom);
+    getDATE(dateEventDom)
 
     function getDATE(str){
         const [date, hour] = str.split("")
