66f4a5a9f426c886b0d7edc05f84db159e8f3ca1
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-18 08:41:16 -0300

    acrecentei mais algumas coisas em meu código js.

--- saveTheDate/js/savethedate.js
+++ saveTheDate/js/savethedate.js
@@ -3,13 +3,14 @@
     console.log(dateEventDom);
    
     const dateEven = getDATE(dateEventDom)
+    console.log(dateEven)
     
-
     function getDATE(str){
         const [date, hour] = str.split("")
-        const [day, month, year] = date.split("7")
-        const [h ,n] = hour.split("H")
-        return new Date (year ,month ,day,h,m)
+        const [day, month, year] = date.split("/")
+        const [h ,m] = hour.split("H")
+      
+        return new Date (year ,month - 1, day,h,m)
     }
 
 })()
