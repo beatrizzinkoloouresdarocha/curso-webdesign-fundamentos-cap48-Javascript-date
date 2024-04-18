cc550aee636cef5826364202c46b910e1499f7df
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-18 11:47:03 -0300

    finalizei mais ajustes  e acrecentei mais informação e fiz ajustes e melhorias em meu código concluida esta etapa e atualizei e sumiu os erros concluidos .

--- saveTheDate/js/savethedate.js
+++ saveTheDate/js/savethedate.js
@@ -1,33 +1,34 @@
 (function () {
     const dateEventDom = document.querySelector(".hero-content h1 span").innerText;
-    console.log(dateEventDom)
+    console.log(dateEventDom);
    
-    const dateEven = getDATE(dateEventDom)
+    const dateEven = getDATE(dateEventDom);
     console.log(dateEven);
 
-    const today = new Date()
+    const today = new Date();
 
-    const left = dateEven.getTime() - today.getTime()
+    let left = dateEven.getTime() - today.getTime();
 
-    const ONE_HOUR =  60 * 60 * 1000
-    const ONE_DAY = 24 * ONE_HOUR 
+    const ONE_HOUR = 60 * 60 * 1000;
+    const ONE_DAY = 24 * ONE_HOUR;
 
-    const daysleft =parseInt (left / ONE_DAY)
-    left = left -daysleft * ONE_DAY
+    const daysleft = Math.floor(left / ONE_DAY);
+    left = left - daysleft * ONE_DAY;
     
-    const daysLeft =parseInt (left /ONE_HOUR )
-    left = left - daysLeft* ONE_HOUR 
-    
-    const hoursLeft  = parseInt(left/ ONE_HOUR )
-    left = left - hoursLeft* ONE_HOUR 
+    const hoursLeft = Math.floor(left / ONE_HOUR);
+    left = left - hoursLeft * ONE_HOUR;
 
+    const minutesLeft = Math.floor(left / (60 * 1000));
+
+    console.log("Days left:", daysleft);
+    console.log("Hours left:", hoursLeft);
+    console.log("Minutes left:", minutesLeft);
 
     function getDATE(str) {
-        const [date, hour] = str.split(" ")
-        const [day, month, year] = date.split("/")
-        const [h, m] = hour.split("H")
+        const [date, hour] = str.split(" ");
+        const [day, month, year] = date.split("/");
+        const [h, m] = hour.split("H");
       
-        return new Date(year, month - 1, day, h, m)
+        return new Date(year, month - 1, day, h, m);
     }
-
 })();
