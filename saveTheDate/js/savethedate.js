e9f2d4f630d11c2a25f96ca3d14db1c86aa10950
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-18 08:56:49 -0300

    fiz ajustes em meu código alterei algumas coisas .

--- saveTheDate/js/savethedate.js
+++ saveTheDate/js/savethedate.js
@@ -1,20 +1,20 @@
 (function () {
     const dateEventDom = document.querySelector(".hero-content h1 span").innerText;
-    console.log(dateEventDom);
+    console.log(dateEventDom)
    
     const dateEven = getDATE(dateEventDom)
     console.log(dateEven)
 
-    const  today = new Date( )
+    const  today = new Date()
 
-    
-    
-    function getDATE(str){
+
+
+    function getDATE(str) {
         const [date, hour] = str.split("")
         const [day, month, year] = date.split("/")
         const [h ,m] = hour.split("H")
       
-        return new Date (year ,month - 1, day,h,m)
+        return new Date (year ,month- 1, day,h,m)
     }
 
 })()
