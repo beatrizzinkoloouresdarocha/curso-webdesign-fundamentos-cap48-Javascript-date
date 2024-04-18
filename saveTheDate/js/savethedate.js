70c16f314cca0e764be26abeb4c65b6674241fb2
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-18 11:13:14 -0300

    fiz mais um ajuste concluido .

--- saveTheDate/js/savethedate.js
+++ saveTheDate/js/savethedate.js
@@ -9,18 +9,18 @@
 
     const left = dateEven.getTime() - today.getTime();
 
-    const ONE_DAY = 24 * 60 * 60 * 1000;
+    const ONE_DAY = 24 * 60 * 60 * 1000
 
-    const daysleft = left / ONE_DAY;
-    console.log(daysleft);
+    const daysleft =parseInt (left / ONE_DAY)
+    console.log(daysleft)
 
 
     function getDATE(str) {
-        const [date, hour] = str.split(" "); // Split by space
-        const [day, month, year] = date.split("/"); // Split by /
-        const [h, m] = hour.split("H"); // Split by H
+        const [date, hour] = str.split(" ")
+        const [day, month, year] = date.split("/")
+        const [h, m] = hour.split("H")
       
-        return new Date(year, month - 1, day, h, m); // Subtract 1 from month
+        return new Date(year, month - 1, day, h, m)
     }
 
 })();
