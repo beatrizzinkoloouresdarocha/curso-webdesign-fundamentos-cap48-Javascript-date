4bcd864458a59dac5d0d87a2f6d8fa20234704e3
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-18 11:38:03 -0300

    fiz algumas alteração e acrecentei  mais  coisas que estavam faltando e fiz ajustes .

--- saveTheDate/js/savethedate.js
+++ saveTheDate/js/savethedate.js
@@ -1,13 +1,13 @@
 (function () {
     const dateEventDom = document.querySelector(".hero-content h1 span").innerText;
-    console.log(dateEventDom);
+    console.log(dateEventDom)
    
-    const dateEven = getDATE(dateEventDom);
+    const dateEven = getDATE(dateEventDom)
     console.log(dateEven);
 
-    const today = new Date();
+    const today = new Date()
 
-    const left = dateEven.getTime() - today.getTime();
+    const left = dateEven.getTime() - today.getTime()
 
     const ONE_HOUR =  60 * 60 * 1000
     const ONE_DAY = 24 * ONE_HOUR 
@@ -15,9 +15,11 @@
     const daysleft =parseInt (left / ONE_DAY)
     left = left -daysleft * ONE_DAY
     
-    const hoursLeft =parseInt (left /ONE_HOUR )
-    left =left - hoursLeft * ONE_HOUR 
+    const daysLeft =parseInt (left /ONE_HOUR )
+    left = left - daysLeft* ONE_HOUR 
     
+    const hoursLeft  = parseInt(left/ ONE_HOUR )
+    left = left - hoursLeft* ONE_HOUR 
 
 
     function getDATE(str) {
