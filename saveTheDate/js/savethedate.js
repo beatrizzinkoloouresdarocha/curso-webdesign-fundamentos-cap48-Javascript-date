80fc26f262a898ee2f3f6dc99cf5b237619db969
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-18 11:06:38 -0300

    fiz a correção do meu código agora funcionou e apareceu as informação que precisava concluida esta etapa.

--- saveTheDate/js/savethedate.js
+++ saveTheDate/js/savethedate.js
@@ -1,26 +1,26 @@
 (function () {
     const dateEventDom = document.querySelector(".hero-content h1 span").innerText;
-    console.log(dateEventDom)
+    console.log(dateEventDom);
    
-    const dateEven = getDATE(dateEventDom)
-    console.log(dateEven)
+    const dateEven = getDATE(dateEventDom);
+    console.log(dateEven);
 
-    const  today = new Date()
+    const today = new Date();
 
-    const  left= dateEven.getTime() - today.getTime ()
+    const left = dateEven.getTime() - today.getTime();
 
-    const ONE_DAY = 24 * 60 * 60 * 1000
+    const ONE_DAY = 24 * 60 * 60 * 1000;
 
-    const daysleft = left /ONE_DAY
-    console.log (daysleft)
+    const daysleft = left / ONE_DAY;
+    console.log(daysleft);
 
 
     function getDATE(str) {
-        const [date, hour] = str.split("")
-        const [day, month, year] = date.split("/")
-        const [h ,m] = hour.split("H")
+        const [date, hour] = str.split(" "); // Split by space
+        const [day, month, year] = date.split("/"); // Split by /
+        const [h, m] = hour.split("H"); // Split by H
       
-        return new Date (year ,month- 1, day,h,m)
+        return new Date(year, month - 1, day, h, m); // Subtract 1 from month
     }
 
-})()
+})();
